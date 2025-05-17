17f771c9db79a414841147fef4e5bb8001a3e19f
D <fardee.tat@gmail.com>
2025-05-18 01:10:56 +0700

    config routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 
 import TSMyFixedSizeList from "./examples/ts/react-window/FixedSizeList.tsx";
 import TSMyFixedSizeGrid from "./examples/ts/react-window/FixedSizeGrid.tsx";
@@ -19,11 +19,14 @@ import JSMyVirtualizedAutoSizer from "./examples/js/react-virtualized/Virtualize
 import JSMyVirtualizedGrid from "./examples/js/react-virtualized/VirtualizedGrid.jsx";
 // @ts-ignore
 import JSMyVirtualizedTable from "./examples/js/react-virtualized/VirtualizedTable.jsx";
+import { useMemo } from 'react';
 
-function App() {
+export default function App() {
 
   return (
     <BrowserRouter>
+      <AppNavigations />
+      
       <Routes>
         {/* typeScript */}
         <Route path="/examples/ts/react-window/fixed-size-list" element={<TSMyFixedSizeList />} />
@@ -44,4 +47,42 @@ function App() {
   )
 }
 
-export default App
+
+function AppNavigations() {
+  const location = useLocation();
+
+  // เช็กว่าหน้านี้เป็นหน้า example หรือไม่
+  const isExamplePage = useMemo(
+    () => location.pathname.startsWith("/examples/"),
+    [location.pathname]
+  );
+
+  if (isExamplePage === true) return null;
+
+  return (
+    <>
+      <div style={{ border: "1px solid red", width: "100vw" }}>
+        <h1 style={{ textAlign: "center" }}>React-window</h1>
+        <nav style={{ display: "flex", justifyContent: "center", gap: "1rem", padding: "1rem" }}>
+          <Link to="/examples/ts/react-window/fixed-size-list">FixedSizeList(TypeScript)</Link>
+          <Link to="/examples/ts/react-window/fixed-size-grid">FixedSizeGrid(TypeScript)</Link>
+          <Link to="/examples/js/react-window/fixed-size-list">FixedSizeList(JavaScript)</Link>
+          <Link to="/examples/js/react-window/fixed-size-grid">FixedSizeGrid(JavaScript)</Link>
+        </nav>
+      </div>
+      <div style={{ border: "1px solid red", width: "100vw" }}>
+        <h1 style={{ textAlign: "center" }}>React-virtualized</h1>
+        <nav style={{ display: "flex", justifyContent: "center", gap: "1rem", padding: "1rem", flexWrap: "wrap" }}>
+          <Link to="/examples/ts/react-virtualized/virtualized-list">VirtualizedList(TypeScript)</Link>
+          <Link to="/examples/ts/react-virtualized/virtualized-auto-sizer">VirtualizedAutoSizer(TypeScript)</Link>
+          <Link to="/examples/ts/react-virtualized/virtualized-grid">VirtualizedGrid(TypeScript)</Link>
+          <Link to="/examples/ts/react-virtualized/virtualized-table">VirtualizedTable(TypeScript)</Link>
+          <Link to="/examples/js/react-virtualized/virtualized-list">VirtualizedList(JavaScript)</Link>
+          <Link to="/examples/js/react-virtualized/virtualized-auto-sizer">VirtualizedAutoSizer(JavaScript)</Link>
+          <Link to="/examples/js/react-virtualized/virtualized-grid">VirtualizedGrid(JavaScript)</Link>
+          <Link to="/examples/js/react-virtualized/virtualized-table">VirtualizedTable(JavaScript)</Link>
+        </nav>
+      </div>
+    </>
+  )
+}
